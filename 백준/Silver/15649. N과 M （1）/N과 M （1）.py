b931a02431acb83191b4d37cf604874c9730b34c
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-03-19 13:12:15 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,28 +1,21 @@
-# N과 M(1)
 import sys
-import itertools
 
 input = sys.stdin.readline
-# N = 자연수
-# M = 중복 없이 M개
+
+def NM(depth, N, i):
+
+    if depth == M:
+
+        print(i.lstrip())
+        return
+
+    for j in range(1, N+1):
+        if not selection[j]:
+            selection[j] = True
+            NM(depth+1, N, str(i) + ' ' + str(j))
+            selection[j] = False
+
 N, M = map(int, input().split())
 
-numbers = list(range(1, N+1))
-
-# print(numbers)
-
-# 부분집합으로 구하기
-# ans1 = []
-# for i in range(1, 1<<N): # 1부터 2**N개의 부분집합 만들어짐
-#     result = []
-#     for j in range(N):
-#         if i & (1<<j): # 조사했는데 만약에 있다.
-#             result.append(numbers[j])
-
-#     if len(result) == M:
-#         ans1.append(result)
-
-
-result = list(itertools.permutations(numbers,M))
-for i in result:
-    print(*i)
+selection = [False] * (N+1)
+NM(0, N, '')
