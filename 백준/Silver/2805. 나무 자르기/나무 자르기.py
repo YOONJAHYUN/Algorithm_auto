97eab5f63c5005d435b5a3ee4519bfa4e074dca3
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-05-05 17:04:47 +0900

    [Silver II] Title: 나무 자르기, Time: 556 ms, Memory: 259740 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+start = 1
+end = max(data)
+
+while start <= end:
+    mid = (start+end)//2
+
+    total = 0
+    for tree in data:
+        if tree >= mid:
+            total += (tree-mid)
+
+    if total >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
