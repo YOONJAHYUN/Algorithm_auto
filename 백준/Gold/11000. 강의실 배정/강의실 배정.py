03a8d22173336b3453aa7781dac0c928ec4c2825
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-05-05 13:18:10 +0900

    [Gold V] Title: 강의실 배정, Time: 340 ms, Memory: 61296 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,22 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+
+n = int(input())
+data = []
+for _ in range(n):
+    s, t = map(int, input().split())
+    data.append((s,t))
+
+data.sort()
+q = []
+# 종료시간을 넣는다.
+heappush(q, data[0][1])
+
+for i in range(1,n):
+    if data[i][0] >= q[0]:
+        heappop(q)
+    heappush(q, data[i][1])
+
+print(len(q))
