84589eb5fd438b94c2949e019bdb72fffb1e6944
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-05-13 16:57:36 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,19 @@
+def solution(brown, yellow):
+    answer = []
+    
+    res = brown + yellow
+    
+    candi = []
+    for i in range(1, res):
+        if int(res/i) == res/i and i >= res/i:
+            candi.append((i, int(res/i)))
+    # print(candi)
+    
+    for x, y in candi:
+        # print((x-1)*(y-1))
+        
+        if (x-2)*(y-2) == yellow:
+            answer = [x, y]
+            break
+    
+    return answer
