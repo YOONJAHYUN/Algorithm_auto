fd3b3aa62dfdf825cd79ea73146fd881b2e42116
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-06-19 22:11:13 +0900

    [Gold IV] Title: 알파벳, Time: 7440 ms, Memory: 188268 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,22 +1,23 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-def DFS(y, x, words):
-    global my_max
-    my_max = max(my_max, words)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < C and 0 <= ny < R and not data[ny][nx] in sett:
-            sett.add(data[ny][nx])
-            DFS(ny, nx, words+1)
-            sett.remove((data[ny][nx]))
+
+def BFS(y, x, word):
+    global ans
+
+    ans = max(ans, len(word))
+
+    for dy, dx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+        ny = y + dy
+        nx = x + dx
+
+        if 0 <= ny < R and 0 <= nx < C and data[ny][nx] not in word:
+            BFS(ny, nx, word+data[ny][nx])
+
 
 R, C = map(int, input().split())
-data = [list(input().rstrip()) for _ in range(R)]
-my_max = 0
-sett = set()
-sett.add(data[0][0])
-DFS(0, 0, 1)
-print(my_max)
+ans = 0
+data = [input().rstrip() for _ in range(R)]
+
+BFS(0, 0, data[0][0])
+print(ans)
