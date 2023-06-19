536441e36c3b26def1996b322e8e8bf4d9113d1a
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2023-06-19 12:15:25 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1304 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -10,17 +10,22 @@ robot = [0] * n
 
 while True:
 
-    tmp1 = data[-1]
-    # 컨베이어벨트 및 로봇 회전 시키기
-    for i in range(2*n-1, -1, -1):
-        data[i] = data[i-1]
-        if i < n:
-            robot[i] = robot[i-1]
+    # tmp1 = data[-1]
+    # # 컨베이어벨트 및 로봇 회전 시키기
+    # for i in range(2*n-1, -1, -1):
+    #     data[i] = data[i-1]
+    #     if i < n:
+    #         robot[i] = robot[i-1]
+    #
+    # robot[-1] = 0
+    # robot[0] = 0
+    # data[0] = tmp1
 
-    robot[-1] = 0
-    robot[0] = 0
-    data[0] = tmp1
-
+    # 문어박사님
+    data.insert(0, data.pop())
+    robot.pop()
+    robot.insert(0, 0)
+    robot[n-1] = 0
 
     # 로봇 회전
     for i in range(n-2, 0, -1):
@@ -41,4 +46,3 @@ while True:
         break
 
     cnt += 1
-    
