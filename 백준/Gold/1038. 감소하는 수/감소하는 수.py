1ab4aaa0b508e227b16ed3bf5bc25de9f86fbbce
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2024-01-29 22:45:38 +0900

    [Gold V] Title: 감소하는 수, Time: 148 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+q = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+while q:
+
+    num = q.pop()
+
+    for i in range(int(str(num)[-1])):
+        new = int(str(num)+str(i))
+        lst.append(new)
+        q.append(new)
+
+lst.sort()
+try:
+    print(lst[n])
+except:
+    print(-1)
+    
