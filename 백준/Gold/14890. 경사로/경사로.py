95ce8eca47ee315e50760bf4f9a4005b3a7a1993
YOONJAHYUN <122499575+YOONJAHYUN@users.noreply.github.com>
2024-03-26 22:23:23 +0900

    [Gold III] Title: 경사로, Time: 144 ms, Memory: 110668 KB -BaekjoonHub

---
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -0,0 +1,79 @@
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = list(map(list, zip(*arr1)))
+visited = [[False] * n for _ in range(n)]
+
+
+def check1(i, j, arr):
+    if visited[i][j]:
+        return False
+
+    # l만큼 상관없는지 확인
+    for k in range(1, l):
+        if j + k >= n:
+            return False
+        if visited[i][j+k]:
+            return False
+
+        if arr[i][j + k] != arr[i][j]:
+            return False
+
+    for k in range(l):
+        visited[i][j+k] = True
+        # arr[i][j+k] += 1
+    return True
+
+def check2(i, j, arr):
+    if visited[i][j]:
+        return False
+    # l만큼 상관없는지 확인
+    for k in range(1, l):
+        if j-k < 0:
+            return False
+        if visited[i][j-k]:
+            return False
+        if arr[i][j - k] != arr[i][j]:
+            return False
+
+    for k in range(l-1):
+        visited[i][j-k] = True
+    #     arr[i][j-k] += 1
+    return True
+
+
+def solution(arr):
+    global answer
+    for i in range(n):
+        for j in range(1, n):
+            # 높이가 전과 같을 경우
+            if arr[i][j] == arr[i][j-1]:
+                continue
+            # 높이 차이가 1인 경우
+            if abs(arr[i][j] - arr[i][j-1]) == 1:
+                temp = check1(i, j, arr) if arr[i][j] < arr[i][j-1] else check2(i, j-1, arr)
+                # 경사로를 못 놓을 경우
+                if not temp:
+                    break
+            else:
+                break
+
+        else:
+            # print(i, "열")
+            answer += 1
+
+answer = 0
+solution(arr1)
+# for i in arr1:
+#     print(i)
+
+visited = [[False] * n for _ in range(n)]
+# print("@@")
+solution(arr2)
+
+# for i in arr2:
+#     print(i)
+
+print(answer)
